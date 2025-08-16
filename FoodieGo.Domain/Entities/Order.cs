0d0c651fdef6a194bec8afffb10dba5a3f6ca0ab
Sharen Rajenthiran <131507266+Sharen-Rajenthiran@users.noreply.github.com>
2025-08-16 20:37:04 +0800

    Update: Added Domain library

---
+++ FoodieGo.Domain/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodieGo.Domain.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int RestaurantId { get; set; }
+        public string Status { get; set; } = "Pending"; // Pending | Preparing | OutForDelivery | Delivered
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
